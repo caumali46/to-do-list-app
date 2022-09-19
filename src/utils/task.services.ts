0aa0760c8c74d4e361032b9403b8b8cee39ee45d
catherineumali <caumali46@gmail.com>
2022-09-20 03:29:33 +0800

    Add all api service

--- src/utils/task.services.ts
+++ src/utils/task.services.ts
@@ -6,3 +6,37 @@ export const getTodoListService = async () => {
     .then((response: any) => Promise.resolve(response))
     .catch((error: any) => Promise.reject(error));
 }
+
+export const deleteTodoService = async (id: number) => {
+  return await fetch(`${config.BASE_URL}/tasks/${id}`,
+    {
+      method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then((response: any) => Promise.resolve(response))
+    .catch((error: any) => Promise.reject(error));
+}
+
+export const addNewTodoService = async (dataObject: object) => {
+  return await fetch(`${config.BASE_URL}`, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+      body: JSON.stringify(dataObject)
+  })
+  .then((response: any) => Promise.resolve(response))
+  .catch((error: any) => Promise.reject(error));
+}
+
+export const completeTodoService = async (id: number, dataObject: object) => {
+  return fetch(`${config.BASE_URL}`,{
+      method:'PUT',
+      headers:{ 'Content-Type':'application/json' },
+      body:JSON.stringify(dataObject)
+    })
+    .then(response => response.json())
+    .then((response: any) => Promise.resolve(response))
+    .catch((error: any) => Promise.reject(error));
+}
